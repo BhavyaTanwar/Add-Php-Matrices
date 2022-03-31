ff176c0c2c28ff10620473e704fe6e7a273b351c
bhavyatanwar <i-bhavya@bluelightsystems.net>
2022-03-31 16:57:36 +0530

    Sum of two 3*3 matrices

--- index.php
+++ index.php
@@ -1,23 +1,23 @@
 <?php
-    $matrice1 = array(
+    $matrix1 = array(
         array(1, 2, 3),
         array(4, 5, 6),
         array(7, 8, 9)
     );
-    $matrice2 = array(
+    $matrix2 = array(
         array(1, 4, 7),
         array(2, 5, 8),
         array(3, 6, 9)
     );
     $sum = array();
-    for ($i = 0; $i < count($matrice1); $i++) {
-        for ($j = 0; $j < count($matrice1[0]); $j++) {
-            $sum[$i][$j] = $matrice1[$i][$j] + $matrice2[$i][$j];
+    for ($i = 0; $i < count($matrix1); $i++) {
+        for ($j = 0; $j < count($matrix1[0]); $j++) {
+            $sum[$i][$j] = $matrix1[$i][$j] + $matrix2[$i][$j];
         }
     }
     echo "Addition of two matrices: "."<br>";
-    for ($i = 0; $i < count($matrice1); $i++) {
-        for ($j = 0; $j < count($matrice1[0]); $j++) {
+    for ($i = 0; $i < count($matrix1); $i++) {
+        for ($j = 0; $j < count($matrix1[0]); $j++) {
             echo $sum[$i][$j] . " ";
         }
         echo "<br>";
